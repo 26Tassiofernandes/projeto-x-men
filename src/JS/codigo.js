2dea4f64702a311906ae5b9d37cc98c55f495254
26TassioFernandes <tassiofernandes881@gmail.com>
2023-07-12 12:29:56 -0300

    Projeto X-men

---
+++ src/JS/codigo.js
@@ -0,0 +1,83 @@
+function ciclope() {
+    nome_personagem = document.getElementById('nome-personagem')
+    descricao = document.getElementById('descricao-personagem')
+    img = document.getElementById('imagem')
+    img.src = 'src/imagens/card-ciclope.png'
+    descricao.innerHTML = 'Ele tem o poder de disparar rajadas ópticas por um acidente que aconteceu com ele quando criança, mas não consegue controlá-los.'
+    nome_personagem.innerHTML = 'Ciclope'
+
+
+}
+
+function jean() {
+    nome_personagem = document.getElementById('nome-personagem')
+    descricao = document.getElementById('descricao-personagem')
+    img = document.getElementById('imagem')
+    img.src = 'src/imagens/card-jean-grey.png'
+    descricao.innerHTML = 'Ela lê mentes e se comunica de forma telepática com outros indivíduos. Sua telecinese permite que ela mova objetos com a força da mente.'
+    nome_personagem.innerHTML = 'Jean Grey'
+
+    
+}
+
+function lince() {
+    nome_personagem = document.getElementById('nome-personagem')
+    descricao = document.getElementById('descricao-personagem')
+    img = document.getElementById('imagem')
+    img.src = 'src/imagens/card-lince-negra.png'
+    descricao.innerHTML = 'Ela pode tornar seu corpo etéreo, passando através de paredes e outros obstáculos, seu combate corpo a corpo é impressionante.'
+    nome_personagem.innerHTML = 'Lince Negra'
+
+    
+}
+
+function tempestade() {
+    nome_personagem = document.getElementById('nome-personagem')
+    descricao = document.getElementById('descricao-personagem')
+    img = document.getElementById('imagem')
+    img.src = 'src/imagens/card-tempestade.png'
+    descricao.innerHTML = ' Ela pode manipular os elementos atmosféricos, criando tempestades, controlando a eletricidade atmosférica e até mesmo gerando raios.'
+    nome_personagem.innerHTML = 'Tempestade'
+
+    
+}
+
+function vampira() {
+    nome_personagem = document.getElementById('nome-personagem')
+    descricao = document.getElementById('descricao-personagem')
+    img = document.getElementById('imagem')
+    img.src = 'src/imagens/card-vampira.png'
+    descricao.innerHTML = 'Ela pode drenar a força vital, os poderes mutantes e as memórias de seus alvos, ganhando temporariamente suas habilidades e conhecimentos.'
+    nome_personagem.innerHTML = 'Vampira'
+
+}
+
+function wolverine() {
+    nome_personagem = document.getElementById('nome-personagem')
+    descricao = document.getElementById('descricao-personagem')
+    img = document.getElementById('imagem')
+    img.src = 'src/imagens/card-wolverine.png'
+    descricao.innerHTML = 'As garras de adamantium, feitas de um metal extremamente resistente, são suas principais armas em combate, tornando-o um lutador letal.'
+    nome_personagem.innerHTML = 'Wolverine'
+    
+}
+
+function noturno() {
+    nome_personagem = document.getElementById('nome-personagem')
+    descricao = document.getElementById('descricao-personagem')
+    img = document.getElementById('imagem')
+    img.src = 'src/imagens/card-noturno.png'
+    descricao.innerHTML = 'Ele pode desaparecer em uma nuvem de fumaça e se teletransportar para locais distantes, aproveitando sua agilidade para realizar ataques surpresa.'
+    nome_personagem.innerHTML = 'Noturno'
+
+}
+
+function magneto() {
+    nome_personagem = document.getElementById('nome-personagem')
+    descricao = document.getElementById('descricao-personagem')
+    img = document.getElementById('imagem')
+    img.src = 'src/imagens/card-magneto.png'
+    descricao.innerHTML = 'Ele pode manipular campos magnéticos para mover objetos metálicos, gerar campos de força e até mesmo controlar partículas metálicas no ar.'
+    nome_personagem.innerHTML = 'Magneto'
+    
+}
